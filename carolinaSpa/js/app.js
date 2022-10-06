d41381db2e117caad8c5377720d35ed4dc5d9fb9
Isaias Carrion <carrionisaias@gmail.com>
2022-10-06 10:22:20 -0300

    curso finalizado

---
+++ carolinaSpa/js/app.js
@@ -0,0 +1,12 @@
+window.swiper = new Swiper({
+  el: ".swiper",
+  slideClass: "swiper-slide",
+  createElements: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  spaceBetween: 20,
+  pagination: true,
+  navigation: true,
+});
